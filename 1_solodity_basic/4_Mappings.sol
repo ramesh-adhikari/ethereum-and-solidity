823ea93986aae80dde0dde5f490f6a078880100e
Adhikari <adrameshadhikari95@gmail.com>
2024-12-28 20:18:41 -0500

    added mapping example

---
+++ 1_solodity_basic/4_Mappings.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Mappings {
+    mapping(uint256 => string) public names;
+    mapping(uint256 => Book) public books;
+    mapping(address => mapping(uint256 => Book)) public myBooks;
+
+    struct Book {
+        string title;
+        string author;
+    }
+
+    constructor() {
+        names[1] = "Adam";
+        names[2] = "Bruce";
+        names[3] = "Carl";
+    }
+
+    function addBook(
+        uint256 _id,
+        string memory _title,
+        string memory _author
+    ) public {
+        books[_id] = Book(_title, _author);
+    }
+
+    function addMyBook(
+        uint256 _id,
+        string memory _title,
+        string memory _author
+    ) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
+    }
+}
