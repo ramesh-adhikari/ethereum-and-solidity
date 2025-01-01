82939d6ab66df0fb1592d0ed98cae0d5bf5b0ad9
Adhikari <adrameshadhikari95@gmail.com>
2025-01-01 18:17:58 -0500

    created RealEstate smart contract

--- 2_real_state_dApp/contracts/RealEstate.sol
+++ 2_real_state_dApp/contracts/RealEstate.sol
@@ -2,6 +2,32 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract RealEstate {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract RealEstate is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("Real Estate", "REAL") {}
+
+    function mint(string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (unit256) {
+        return _tokenIds.current(); //return currently maintained nft
+    }
 
 }
