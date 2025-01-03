bf56d032853c4b12444b1b7b59b52a496fe653d7
Adhikari <adrameshadhikari95@gmail.com>
2025-01-02 19:50:21 -0500

    added only owner can see the list

--- 2_real_state_dApp/contracts/Escrow.sol
+++ 2_real_state_dApp/contracts/Escrow.sol
@@ -10,11 +10,23 @@ interface IERC721 {
     ) external;
 }
 
+
+
 contract Escrow {
     address public nftAddress;
     address payable public seller;
     address public lender;
     address public inspector;
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call ths method");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;  //To store the status of the listed property
+    mapping(uint256 => uint256) public purchasePrice; 
+    mapping(uint256 => uint) public escrowAmount; 
+    mapping(uint256 => address) public buyer; 
 
     constructor(address _nftAddress, address payable _seller, address _inspector, address _lender) {
         nftAddress = _nftAddress;
@@ -23,7 +35,19 @@ contract Escrow {
         lender = _lender;
     }
 
-    function list(uint256 _nftID) public {
+
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        // Transfer NFT from seller to this contract
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
     }
 }
